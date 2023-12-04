a0c508266c1fb22139e22b8bbadf9b2266592a59
astridnadiaa <astridkartikasari21@gmail.com>
2023-12-04 14:36:32 +0700

    jobsheet5

---
+++ jobsheet5/views/mahasiswa/tambah.php
@@ -0,0 +1,101 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SIAKAD</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SIAKAD</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<div class="px-3 py-3"> 
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">SIAKAD</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="tampil_mhs.php">Mahasiswa</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="tampil_dosen.php">Dosen</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<h3>Tambah Data Mahasiswa</h3>
+<form action="proses_tambah.php" method="post">
+    <table>
+        <tr>
+            <td>NIM</td>
+            <td><input type="text" name="nim"></td>
+        </tr>
+        <tr>
+            <td>Nama</td>
+            <td><input type="text" name="nama"></td>
+        </tr>
+        <tr>
+            <td>Tempat Lahir</td>
+            <td><input type="text" name="tempat_lahir"></td>
+        </tr>
+        <tr>
+            <td>Tanggal Lahir</td>
+            <td><input type="date" name="tanggal_lahir"></td>
+        </tr>
+        <tr>
+            <td>Jenis Kelamin</td>
+            <td>
+                <select name="jenis_kelamin">
+                    <option value="L">Laki-laki</option>
+                    <option value="P">Perempuan</option>
+                </select>
+            </td>
+        </tr>
+        <tr>
+            <td>Agama</td>
+            <td>
+                <select name="agama">
+                    <option value="islam">Islam</option>
+                    <option value="kristen">Kristen</option>
+                    <option value="katolik">Katolik</option>
+                    <option value="hindu">Hindu</option>
+                    <option value="budha">Budha</option>
+                    <option value="konghucu">Konghucu</option>
+                </select>
+            </td>
+        </tr>
+        <tr>
+            <td>Alamat</td>
+            <td>
+                <textarea name="alamat" cols="30" rows="5"></textarea>
+            </td>
+        </tr>
+        <tr>
+        <tr>
+            <td></td>
+            <td><input class="btn btn-success mb-3 mt-2" type="submit" name="submit" value="Simpan"></td>
+            </tr>
+        </tr>
+    </table>
+</form>
+
+  <script>
+    function showAlert() {
+      alert ("Data Mahasiswa berhasil ditambahkan")
+    }
+  </script>
+</div>
